a39f91542233acc660c97a0d0895c9e7d9b62eef
Ian Vernon <ian@cilium.io>
2017-12-14 21:16:47 -0800

    download deb instead of apt-get

--- tests/k8s/cluster/cluster-manager.bash
+++ tests/k8s/cluster/cluster-manager.bash
@@ -179,27 +179,9 @@ EOF
 }
 
 function install_kubeadm_dependencies(){
-    sudo touch /etc/apt/sources.list.d/kubernetes.list
-    curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg  | sudo apt-key add -
-    sudo bash -c "cat <<EOF > /etc/apt/sources.list.d/kubernetes.list
-deb http://apt.kubernetes.io/ kubernetes-xenial main
-EOF
-"
-    sudo apt-get -qq update
-    sudo apt-get -y -qq install \
-      apt-transport-https \
-      ca-certificates \
-      curl \
-      software-properties-common
-    
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-    sudo add-apt-repository \
-      "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-      $(lsb_release -cs) \
-      stable"
-
-    sudo apt-get update
-    sudo apt-get install -y docker-ce
+    wget https://download.docker.com/linux/ubuntu/dists/xenial/pool/stable/amd64/docker-ce_17.09.1~ce-0~ubuntu_amd64.deb -O /tmp/docker-ce_17.09.1~ce-0~ubuntu_amd64.deb
+  sudo dpkg -i /tmp/docker-ce_17.09.1~ce-0~ubuntu_amd64.deb
+    rm /tmp/docker-ce_17.09.1~ce-0~ubuntu_amd64.deb
     sudo usermod -aG docker vagrant
 }
 
