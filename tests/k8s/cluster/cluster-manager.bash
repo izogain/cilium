15ba7b06351272f1e91b8057a0b54f64cf86cae5
Ian Vernon <ian@cilium.io>
2017-12-14 21:33:36 -0800

    again

--- tests/k8s/cluster/cluster-manager.bash
+++ tests/k8s/cluster/cluster-manager.bash
@@ -179,9 +179,17 @@ EOF
 }
 
 function install_kubeadm_dependencies(){
-    sudo apt-get install libltdl7
+    sudo touch /etc/apt/sources.list.d/kubernetes.list
+    curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg  | sudo apt-key add -
+    sudo bash -c "cat <<EOF > /etc/apt/sources.list.d/kubernetes.list
+deb http://apt.kubernetes.io/ kubernetes-xenial main
+EOF
+"
+    sudo apt-get -qq update && sudo apt-get -qq install -y apt-transport-https
+
+    sudo apt-get install -qq -y libltdl7
     wget https://download.docker.com/linux/ubuntu/dists/xenial/pool/stable/amd64/docker-ce_17.09.1~ce-0~ubuntu_amd64.deb -O /tmp/docker-ce_17.09.1~ce-0~ubuntu_amd64.deb
-  sudo dpkg -i /tmp/docker-ce_17.09.1~ce-0~ubuntu_amd64.deb
+    sudo dpkg -i /tmp/docker-ce_17.09.1~ce-0~ubuntu_amd64.deb
     rm /tmp/docker-ce_17.09.1~ce-0~ubuntu_amd64.deb
     sudo usermod -aG docker vagrant
 }
